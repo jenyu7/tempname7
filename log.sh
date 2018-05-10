be85dfcdc22b7bc1036233791c17fea18e585aab
Charles <cweng@stuy.edu>
2018-05-10 13:02:41 -0400

    things stuffs

--- log.sh
+++ log.sh
@@ -3,13 +3,13 @@
 #CREDITS TO James Smith
 
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-ENTRY="$1 -- $DATE\n$2\n"
+ENTRY="\n$1 -- $DATE\n$2"
 
 if [ $# -eq 2 ]; then
     if [ ! -e devlog.txt ]; then
         touch devlog.txt
     fi
-    echo -e "$ENTRY" >> devlog.txt   
+    echo -e "$ENTRY" >> devlog.txt
 else
-    echo -e "Incorrect number of arguments supplied\nUsage: $ ./log.sh <firstL> <message>"  
+    echo -e "Incorrect number of arguments supplied\nUsage: $ ./log.sh <firstL> <message>"
 fi
