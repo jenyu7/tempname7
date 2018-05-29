936dfa5e5144eae9b514278bfae72b4cec8889e2
Jennifer Zhang <jennifer.newyork@aol.com>
2018-05-29 13:19:48 -0400

    started on chance cards

--- tempname7/utils/db.py
+++ tempname7/utils/db.py
@@ -166,13 +166,34 @@ def init_board(c, populate=True):
 def init_cards(c, populate=True):
     c.execute("CREATE TABLE chance_cards(id INTEGER PRIMARY KEY, description TEXT)")
     c.execute("CREATE TABLE comm_cards(id INTEGER PRIMARY KEY, description TEXT)")
-    '''
+    
     if populate:
+        #populating chance cards
+
+        chance = [
+            'Advance to Go (Collect $200)',
+            'Advance to Illinois Ave. - If you pass Go, collect $200',
+            'Advance to St. Charles Place – If you pass Go, collect $200',
+            'Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.',
+            'Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.',
+            'Bank pays you dividend of $50',
+            'Get out of Jail Free – This card may be kept until needed, or traded/sold',
+            'Go Back 3 Spaces',
+            'Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200',
+            'Make general repairs on all your property – For each house pay $25 – For each hotel $100',
+            'Pay poor tax of $15 ',
+            'Take a trip to Reading Railroad – If you pass Go, collect $200',
+            'Take a walk on the Boardwalk – Advance token to Boardwalk.',
+            'You have been elected Chairman of the Board – Pay each player $50',
+            'Your building and loan matures – Collect $150',
+            'You have won a crossword competition - Collect $100'
+            ]
+        
+        '''
         comm = 'INSERT INTO chance_cards VALUES(%d, "%s")' %(id, description)
         comm = 'INSERT INTO comm_cards VALUES(%d, "%s")' %(id, description)
         c.execute(comm)
-    '''
-
+        '''
 
 #===============================TESTS===============================
 '''
@@ -199,7 +220,8 @@ print auth("jenni", "123452")
 '''
 
     
-#db = sqlite3.connect('data.db')
-#c = db.cursor()
+db = sqlite3.connect('data.db')
+c = db.cursor()
 
 #init_board(c)
+init_cards(c)
