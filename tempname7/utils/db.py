fd6806adf09bc83865998802e4cad10b4b49548e
Jennifer Zhang <jennifer.newyork@aol.com>
2018-05-30 11:26:36 -0400

    community cards

--- tempname7/utils/db.py
+++ tempname7/utils/db.py
@@ -158,7 +158,6 @@ def init_board(c, populate=True):
         for b in board:
             comm = 'INSERT INTO board VALUES(%d, "%s", "%s", %d)' %(b[0], b[1], b[2], b[3])
             c.execute(comm)
-            #print b
 
             
 
@@ -169,31 +168,59 @@ def init_cards(c, populate=True):
     
     if populate:
         #populating chance cards
-
         chance = [
             'Advance to Go (Collect $200)',
             'Advance to Illinois Ave. - If you pass Go, collect $200',
-            'Advance to St. Charles Place – If you pass Go, collect $200',
+            'Advance to St. Charles Place - If you pass Go, collect $200',
             'Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.',
             'Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.',
             'Bank pays you dividend of $50',
-            'Get out of Jail Free – This card may be kept until needed, or traded/sold',
+            'Get out of Jail Free - This card may be kept until needed, or traded/sold',
             'Go Back 3 Spaces',
-            'Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200',
-            'Make general repairs on all your property – For each house pay $25 – For each hotel $100',
+            'Go to Jail - Go directly to Jail - Do not pass Go, do not collect $200',
+            'Make general repairs on all your property - For each house pay $25 - For each hotel $100',
             'Pay poor tax of $15 ',
-            'Take a trip to Reading Railroad – If you pass Go, collect $200',
-            'Take a walk on the Boardwalk – Advance token to Boardwalk.',
-            'You have been elected Chairman of the Board – Pay each player $50',
-            'Your building and loan matures – Collect $150',
+            'Take a trip to Reading Railroad - If you pass Go, collect $200',
+            'Take a walk on the Boardwalk - Advance token to Boardwalk.',
+            'You have been elected Chairman of the Board - Pay each player $50',
+            'Your building and loan matures - Collect $150',
             'You have won a crossword competition - Collect $100'
             ]
-        
-        '''
-        comm = 'INSERT INTO chance_cards VALUES(%d, "%s")' %(id, description)
-        comm = 'INSERT INTO comm_cards VALUES(%d, "%s")' %(id, description)
-        c.execute(comm)
-        '''
+            
+        i = 0
+        while i < len(chance):
+            comm = 'INSERT INTO chance_cards VALUES(%d, "%s")' %(i, chance[i])
+            c.execute(comm)
+            i+=1
+
+            
+        #populating community cards
+        commun = [
+            'Advance to Go (Collect $200)',
+            'Bank error in your favor - Collect $200',
+            "Doctor's fees - Pay $50",
+            'From sale of stock you get $50',
+            'Get Out of Jail Free - This card may be kept until needed or sold',
+            'Go to Jail - Go directly to jail - Do not pass Go - Do not collect $200',
+            'Grand Opera Night - Collect $50 from every player for opening night seats',
+            'Holiday Fund matures - Receive $100',
+            'Income tax refund - Collect $20',
+            'It is your birthday - Collect $10 from each player',
+            'Life insurance matures - Collect $100',
+            'Pay hospital fees of $100',
+            'Pay school fees of $150',
+            'Receive $25 consultancy fee',
+            'You are assessed for street repairs - $40 per house - $115 per hotel',
+            'You have won second prize in a beauty contest - Collect $10',
+            'You inherit $100'
+            ]
+       
+        i = 0
+        while i < len(commun):
+            comm = 'INSERT INTO comm_cards VALUES(%d, "%s")' %(i, commun[i])
+            c.execute(comm)
+            i+=1
+
 
 #===============================TESTS===============================
 '''
@@ -220,8 +247,8 @@ print auth("jenni", "123452")
 '''
 
     
-db = sqlite3.connect('data.db')
-c = db.cursor()
+#db = sqlite3.connect('data.db')
+#c = db.cursor()
 
 #init_board(c)
-init_cards(c)
+#init_cards(c)
